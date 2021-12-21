c444b38b60d39186419296c655a65caf581bd91d
vfz <stavfz@gmail.com>
2021-12-21 23:47:51 +0300

    add ZIP for compressin All generate file to .zip

--- index.php
+++ index.php
@@ -74,7 +74,7 @@ if(empty($my_width)){
 6400x4800
 7680x4320
 7680x4800
-10240x5760 
+10240x5760
 11520x6480</textarea>
         <input type=submit value='Сгенерировать файлы'>
     </form>
@@ -92,9 +92,26 @@ echo"
 ";
 echo "</td><td>";
 $url = ((!empty($_SERVER['HTTPS'])) ? 'https' : 'http') . '://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-foreach($array_resolv AS $my_resolve){
-    $results_pecent_without_fraction="";
-    $results_pecent_with_fraction="";
+
+@unlink(__DIR__."/All_resolve_without_fraction.zip");
+@unlink(__DIR__."/All_resolve_with_fraction.zip");
+
+$zip = new ZipArchive;
+$filename = __DIR__."/All_resolve_without_fraction.zip";
+if ($zip->open($filename, ZipArchive::CREATE)!==TRUE) {
+    exit("Невозможно открыть <$filename>\n");
+}
+
+$zip2 = new ZipArchive;
+$filename2 = __DIR__."/All_resolve_with_fraction.zip";
+if ($zip2->open($filename2, ZipArchive::CREATE)!==TRUE) {
+    exit("Невозможно открыть <$filename2>\n");
+}
+
+foreach($array_resolv AS $my_resolv){
+    $results_pecent_without_fraction="0";
+    $results_pecent_with_fraction="0";
+    $my_resolve=trim($my_resolv);
     for($i=0.01; $i<0.99; $i=$i+0.01)
     {
         $pos_start = mb_strpos($my_resolve, "x", 0);
@@ -106,12 +123,24 @@ foreach($array_resolv AS $my_resolve){
         $results_pecent_without_fraction.="
 ".$results_without_fraction;    
     }
+    $results_pecent_without_fraction.="
+".$my_width_set;
+        $results_pecent_with_fraction.="
+".$my_width_set;
 
     file_put_contents('Resolve_without_fraction_'.$my_resolve.'.TXT',$results_pecent_without_fraction);
+    $zip->addFile("Resolve_without_fraction_".$my_resolve.".TXT");
     file_put_contents('Resolve_with_fraction_'.$my_resolve.'.TXT',$results_pecent_with_fraction);
-    echo '<a href="'.$url.'Resolve_without_fraction_'.$my_resolve.'.TXT">'.$url.'Resolve_without_fraction_'.$my_resolve.'.TXT</a><br>';
-    echo '<a href="'.$url.'Resolve_with_fraction_'.$my_resolve.'.TXT">'.$url.'Resolve_with_fraction_'.$my_resolve.'.TXT</a><br>';
+    $zip2->addFile("Resolve_with_fraction_".$my_resolve.".TXT");
+    // echo '<a href="'.$url.'Resolve_without_fraction_'.$my_resolve.'.TXT">'.$url.'Resolve_without_fraction_'.$my_resolve.'.TXT</a><br>';
+    // echo '<a href="'.$url.'Resolve_with_fraction_'.$my_resolve.'.TXT">'.$url.'Resolve_with_fraction_'.$my_resolve.'.TXT</a><br>';
 
 }
-echo "</td></tr></table>";
+echo '<a href="'.$url.'All_resolve_without_fraction.zip">'.$url.'All_resolve_without_fraction.zip</a><br>';
+echo '<a href="'.$url.'All_resolve_with_fraction.zip">'.$url.'All_resolve_with_fraction.zip</a><br>';
+echo "
+</td></tr></table>";
+
+    $zip->close();
+    $zip2->close();
 ?>
